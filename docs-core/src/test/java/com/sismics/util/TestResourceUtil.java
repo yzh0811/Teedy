9d3cd031c292e59c6c771722235f50aac91ab8d2
Zhehao Yang <12210529@mail.sustech.edu.cn>
2025-04-30 14:42:22 +0800

    更新

--- docs-core/src/test/java/com/sismics/util/TestResourceUtil.java
+++ docs-core/src/test/java/com/sismics/util/TestResourceUtil.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * Test of the resource utils.
  *
- * @author jtremeaux 
+ * @author jtremeaux
  */
 public class TestResourceUtil {
 
@@ -25,5 +25,23 @@ public class TestResourceUtil {
 
         fileList = ResourceUtil.list(Test.class, "junit/framework/");
         Assert.assertTrue(fileList.contains("Test.class"));
+
+        fileList = ResourceUtil.list(Test.class, "");
+        Assert.assertEquals(0, fileList.size());
+
+        fileList = ResourceUtil.list(Test.class, "/nonexistent/path");
+        Assert.assertEquals(0, fileList.size());
+    }
+    // 测试ResourceUtil.list方法传入非法参数时是否抛出异常
+    @Test(expected = IllegalArgumentException.class)
+    public void listFilesWithIllegalArgumentTest() throws Exception {
+        ResourceUtil.list(null, "/junit/framework");
+    }
+
+    // 假设ResourceUtil有构造函数，测试构造函数
+    @Test
+    public void resourceUtilConstructorTest() {
+        ResourceUtil resourceUtil = new ResourceUtil();
+        Assert.assertNotNull(resourceUtil);
     }
 }
